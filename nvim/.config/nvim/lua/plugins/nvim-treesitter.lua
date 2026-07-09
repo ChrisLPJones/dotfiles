792fa30fb6662e9c1744632af490b6bda349d058
Christian Jones <136823482+ChrisLPJones@users.noreply.github.com>
2026-07-10 03:11:33 +1000

    Update nvim-treesitter configuration and dependencies

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,132 +1,79 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		branch = "main",
-		lazy = false,
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter").setup({
-				install_dir = vim.fn.stdpath("data") .. "/site",
-			})
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
+  },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua", "c", "c_sharp", "typescript", "javascript", "tsx",
+        "html", "css", "json", "yaml", "bash", "python",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["ai"] = "@conditional.outer",
+            ["ii"] = "@conditional.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]c"] = "@class.outer",
+            ["]a"] = "@parameter.inner",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]C"] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[c"] = "@class.outer",
+            ["[a"] = "@parameter.inner",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[C"] = "@class.outer",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = { ["<leader>a"] = "@parameter.inner" },
+          swap_previous = { ["<leader>A"] = "@parameter.inner" },
+        },
+      },
+    })
 
-			-- Install parsers if missing
-			local parsers = {
-				"lua",
-				"c",
-				"c_sharp",
-				"typescript",
-				"javascript",
-				"tsx",
-				"html",
-				"css",
-				"json",
-				"yaml",
-				"bash",
-				"python",
-			}
-			require("nvim-treesitter").install(parsers)
-
-			-- Enable highlighting + folding per filetype
-			-- (map parser name -> filetypes Neovim uses)
-			local ft_map = {
-				lua = "lua",
-				c = "c",
-				c_sharp = "cs",
-				typescript = "typescript",
-				javascript = "javascript",
-				tsx = "typescriptreact",
-				html = "html",
-				css = "css",
-				json = "json",
-				yaml = "yaml",
-				bash = "sh",
-				python = "python",
-			}
-			local filetypes = vim.tbl_values(ft_map)
-
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = filetypes,
-				callback = function(args)
-					pcall(vim.treesitter.start, args.buf)
-					vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-					vim.wo.foldmethod = "expr"
-					vim.wo.foldenable = false -- don't auto-fold on open; remove if you like folds closed
-				end,
-			})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		branch = "main",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		config = function()
-			local select = require("nvim-treesitter-textobjects.select")
-			local move = require("nvim-treesitter-textobjects.move")
-			local swap = require("nvim-treesitter-textobjects.swap")
-
-			-- Select
-			local sel_map = {
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
-				["aa"] = "@parameter.outer",
-				["ia"] = "@parameter.inner",
-				["ai"] = "@conditional.outer",
-				["ii"] = "@conditional.inner",
-				["al"] = "@loop.outer",
-				["il"] = "@loop.inner",
-			}
-			for key, query in pairs(sel_map) do
-				vim.keymap.set({ "x", "o" }, key, function()
-					select.select_textobject(query, "textobjects")
-				end, { desc = "TS select " .. query })
-			end
-
-			-- Move
-			local function mv(fn, query, desc)
-				return function()
-					fn(query, "textobjects")
-				end, { desc = desc }
-			end
-			vim.keymap.set({ "n", "x", "o" }, "]f", function()
-				move.goto_next_start("@function.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "]c", function()
-				move.goto_next_start("@class.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "]a", function()
-				move.goto_next_start("@parameter.inner", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "]F", function()
-				move.goto_next_end("@function.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "]C", function()
-				move.goto_next_end("@class.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "[f", function()
-				move.goto_previous_start("@function.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "[c", function()
-				move.goto_previous_start("@class.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "[a", function()
-				move.goto_previous_start("@parameter.inner", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "[F", function()
-				move.goto_previous_end("@function.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "[C", function()
-				move.goto_previous_end("@class.outer", "textobjects")
-			end)
-
-			-- Swap
-			vim.keymap.set("n", "<leader>a", function()
-				swap.swap_next("@parameter.inner")
-			end, { desc = "Swap next param" })
-			vim.keymap.set("n", "<leader>A", function()
-				swap.swap_previous("@parameter.inner")
-			end, { desc = "Swap prev param" })
-		end,
-	},
+    -- Treesitter folding (highlighting is handled by highlight.enable above)
+    local fold_fts = {
+      "lua", "c", "cs", "typescript", "javascript", "typescriptreact",
+      "html", "css", "json", "yaml", "sh", "python",
+    }
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = fold_fts,
+      callback = function()
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldenable = false
+      end,
+    })
+  end,
 }
